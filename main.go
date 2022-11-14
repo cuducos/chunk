bd0e41af63a15e3a786b281114d9baf0d3508aaf
Vinnie Mesel <bioinfo.vinicius@gmail.com>
2022-11-14 14:45:57 -0300

    Better error handling, as suggested by @cuducos
    Co-authored-by: Eduardo Cuducos <4732915+cuducos@users.noreply.github.com>

--- main.go
+++ main.go
@@ -136,8 +136,7 @@ func (d *Downloader) downloadFileWithTimeout(userCtx context.Context, u string) 
 func (d *Downloader) getContentSizeHeader(ctx context.Context, u string) (uint64, error) {
 	req, err := http.NewRequestWithContext(ctx, http.MethodHead, u, nil)
 	if err != nil {
-		err = fmt.Errorf("error creating the request for %s: %w", u, err)
-		return 0, err
+		return 0, fmt.Errorf("error creating the request for %s: %w", u, err)
 	}
 	resp, err := d.client.Do(req)
 	if err != nil {
